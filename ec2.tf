8260563cdf106aabb6a7452c3aaf471abf3be151
Nogutsune <arpitsingh23sep@gmail.com>
2018-09-29 19:22:59 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-04681a1dbd79675a5"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "HelloWorld"
+  }
+}
